965b40f7a0604562bcebacdca87789b45abff5c9
LokiLeiche <127683587+LokiLeiche@users.noreply.github.com>
2026-07-20 16:19:37 +0200

    automatically build profile SVG

--- src/index.ts
+++ src/index.ts
@@ -128,4 +128,7 @@ async function main() {
     console.log("✓ Successfully generated terminal.svg");
 }
 
-main().catch(console.error);
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
